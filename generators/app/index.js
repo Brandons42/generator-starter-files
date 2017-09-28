81701502ea4f319a8b7d126976f4dedee783310c
Brandon <brandonbsuen@gmail.com>
2017-09-27 20:55:25 -0700

    .gitattributes added

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,81 +5,26 @@ const yosay = require('yosay');
 
 module.exports = class extends Generator {
 
-  constructor(args, opts) {
-
-    super(args, opts);
-
-    this.option('bower', {
-      alias: 'b',
-      desc: 'Use Bower for dependency management',
-      type: String
-    });
-
-    this.option('npm', {
-      alias: 'n',
-      desc: 'Use NPM for dependency management',
-      type: String
-    });
-
-    this.option('noYarn', {
-      alias: 'ny',
-      desc: 'Use Yarn for dependency management',
-      type: String
-    });
-
-  }
-
-  initializing() {
-
-    //this.composeWith(require.resolve('generator-gitattributes/app'));
-
-  }
-
   prompting() {
-    // Have Yeoman greet the user.
+
     this.log(yosay(
       'Welcome to the ' + chalk.red('generator-starter-files') + ' generator!'
     ));
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
   }
 
   writing() {
 
-    let ignored;
-    if ((!!(this.options.bower) && !(this.options.noYarn)) || (!!(this.options.bower) && !!(this.options.npm))) {
-      ignored = "bower_components\nnode_modules";
-    }
-    else if (!(this.options.noYarn) || !!(this.options.npm)) {
-      ignored = "node_modules";
-    }
-    else if (!!(this.options.bower)) {
-      ignored = "bower_components";
-    }
-
     this.fs.copy(
       this.templatePath('gitignore'),
-      this.destinationPath('./.gitignore'), {
-        ignored: ignored
-    });
+      this.destinationPath('./.gitignore')
+    );
+
+    this.fs.copy(
+      this.templatePath('gitattributes'),
+      this.destinationPath('./.gitattributes')
+    );
 
   }
 
-  install() {
-    this.installDependencies({
-      bower: !!(this.options.bower),
-      npm: !!(this.options.npm),
-      yarn: !(this.options.noYarn)
-    });
-  }
 };
